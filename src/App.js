b8ec9635033a7ae9d1a35488f3964744805b9b40
oleksiivelychko <alexoleksiivelychko@gmail.com>
2021-02-12 20:15:23 +0200

    add bootstrap example login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React from "react";
-
-import {
-    BrowserRouter,
-    Switch,
-    Route, Link
-} from "react-router-dom";
+import {BrowserRouter, Switch, Route, Link} from "react-router-dom";
 import HomePage from "./components/pages/HomePage";
 import LoginPage from "./components/pages/LoginPage";
 
@@ -12,10 +7,6 @@ export default class App extends React.Component {
     render() {
         return (
             <BrowserRouter>
-                <div>
-                    <Link to="/">Home</Link>
-                    <Link to="/login">Login</Link>
-                </div>
                 <Switch>
                     <Route path="/" component={HomePage} exact />
                     <Route path="/login" component={LoginPage} />
--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 export default class LoginPage extends React.Component {
+
     constructor(props) {
         super(props);
     }
+
     render(){
         return (
-            <div className="content">
-                <h1>Login page</h1>
+            <div className="auth">
+                <form className="form-signin">
+                    <h1 className="h3 mb-3 font-weight-normal">Sign in</h1>
+                    <label htmlFor="inputEmail" className="sr-only">Email</label>
+                    <input type="email" id="inputEmail" className="form-control" placeholder="Email" required autoFocus/>
+                    <label htmlFor="inputPassword" className="sr-only">Password</label>
+                    <input type="password" id="inputPassword" className="form-control" placeholder="Password" required/>
+                    <button className="btn btn-lg btn-primary btn-block" type="submit">Log in</button>
+                    <p className="mt-4 mb-3 text-muted"><Link to='/'>Go Back</Link></p>
+                </form>
             </div>
         );
     }
