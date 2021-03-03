bca23725bf25226c5df7baa3317893f848b4cef8
oleksiivelychko <alexoleksiivelychko@gmail.com>
2021-03-03 09:45:27 +0200

    remove useless constructor

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -2,9 +2,6 @@ import React from 'react';
 import {Link} from "react-router-dom";
 
 export default class HomePage extends React.Component {
-    constructor(props) {
-        super(props);
-    }
     render(){
         return (
             <div className="content">
--- src/components/pages/auth/LoginPage.js
+++ src/components/pages/auth/LoginPage.js
@@ -2,11 +2,6 @@ import React from 'react';
 import {Link} from "react-router-dom";
 
 export default class LoginPage extends React.Component {
-
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return (
             <div className="auth">
